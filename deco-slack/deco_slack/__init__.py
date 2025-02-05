e29715090c8eba775663720a127d8cf3f010727e
taross-f <taro.furuya@gmail.com>
2025-02-06 00:03:07 +0900

    style: 末尾の空白を削除

--- deco-slack/deco_slack/__init__.py
+++ deco-slack/deco_slack/__init__.py
@@ -110,7 +110,7 @@ def deco_slack(**kwargs):
                     message = _create_message(kwargs["error"], e)
                     if "stacktrace" in kwargs["error"]:
                         message["text"] = (
-                            message.get("text", "") + 
+                            message.get("text", "") +
                             f"\n```{traceback.format_exc()}```"
                         )
                     _call_func_if_set(message, *args)
